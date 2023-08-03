4e77593fa3ff90acdfd2a4df0bd5ac3e3de61e7e
PHANITHAPU LAKSHMANA RAO <117673383+LakshmanP-111@users.noreply.github.com>
2023-08-03 11:26:22 +0530

    Folder Structure Updated

--- 2.Gram_Vikas (MF)/gram_vikas/src/app/app-routing.module.ts
+++ 2.Gram_Vikas (MF)/gram_vikas/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponentAuth } from 'projects/authentication/src/app/app.component';
-import { DahshboardComponent } from 'projects/dash-board/src/app/app.component';
+import { AppComponentAuth } from 'projects/Data_Collector/authentication/src/app/app.component';
+import { DahshboardComponent } from 'projects/Data_Collector/dash-board/src/app/app.component';
+import { ReportsComponent } from 'projects/Data_Collector/reports/src/app/app.component';
+import { VillagesComponent } from 'projects/Data_Collector/village/src/app/app.component';
 import { MainAppComponent } from './app.component';
-import { ReportsComponent } from 'projects/reports/src/app/app.component';
-import { VillagesComponent } from 'projects/village/src/app/app.component';
+
 
 const routes: Routes = [
   {
     path: 'auth', component: AppComponentAuth
   },
-  { path: '', component: MainAppComponent },
+  {
+    path: '', component: MainAppComponent
+  },
   {
     path: 'dashboard', component: DahshboardComponent
   },
